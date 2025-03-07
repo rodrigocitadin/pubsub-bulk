58cd6abec944e8921fc890ce85c368dd00453ced
rodrigocitadin <citadinrodrigo@gmail.com>
2025-03-06 22:19:32 -0300

    feat: basic usage in of consumer and producer in main

--- main.go
+++ main.go
@@ -2,28 +2,62 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"rodrigocitadin/pubsub-bulk/consumer"
 	"rodrigocitadin/pubsub-bulk/producer"
 	"syscall"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 func main() {
-	os.Setenv("KAFKA_BROKERS", "localhost:9092")
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-
 	ctx, cancel := context.WithCancel(context.Background())
 
-	go consumer.Start(ctx)
-	go producer.Start(ctx)
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
 
-	<-quit
-	fmt.Println("Leaving the program...")
+	producer := producer.New([]string{"localhost:9092"}, "my-topic", ctx)
+	consumer := consumer.New([]string{"localhost:9092"}, "my-topic", "my-group", ctx)
+
+	go startProducer(producer)
+	go startConsumer(consumer)
+
+	<-sig
+	log.Println("Exiting the application...")
+	producer.Close()
+	consumer.Close()
 	cancel()
-	time.Sleep(1 * time.Second)
+}
+
+func startProducer(p producer.Producer) {
+	for {
+		uuid, err := uuid.NewV7()
+		if err != nil {
+			log.Fatal("Error creating UUID V7")
+		}
+
+		if err := p.WriteMessage(uuid.String(), []byte("Random Message")); err != nil {
+			log.Fatalf("Message %v did not arrive", uuid.String())
+		}
+		log.Printf("Message %v arrived\n", uuid.String())
+
+		time.Sleep(time.Second)
+	}
+}
+
+func startConsumer(c consumer.Consumer) {
+	for {
+		if msgs, err := c.ReadMessageBatch(5, 10); err != nil {
+			log.Fatal("Error reading batch of messages")
+		} else {
+			log.Printf("Successful batch reading of %d messages\n", len(*msgs))
+
+			for _, msg := range *msgs {
+				log.Printf("Message %v within batch", string(msg.Key))
+			}
+		}
+	}
 }
