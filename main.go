569bc3b1571fa0503edd665775d3444ff5cbe4b2
rodrigocitadin <citadinrodrigo@gmail.com>
2025-03-08 15:11:55 -0300

    wip

--- main.go
+++ main.go
@@ -6,11 +6,21 @@ import (
 	"os"
 	"os/signal"
 	"rodrigocitadin/pubsub-bulk/consumer"
+	"rodrigocitadin/pubsub-bulk/database"
 	"rodrigocitadin/pubsub-bulk/producer"
+	"rodrigocitadin/pubsub-bulk/proto"
 	"syscall"
 	"time"
 
 	"github.com/google/uuid"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	postgresURL = "host=localhost user=pubsub password=pubsub dbname=pubsub port=5432 sslmode=disable"
+	kafkaURL    = "localhost:9092"
+	kafkaTopic  = "my-topic"
+	kafkaGroup  = "my-group"
 )
 
 func main() {
@@ -19,11 +29,16 @@ func main() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
 
-	producer := producer.New([]string{"localhost:9092"}, "my-topic", ctx)
-	consumer := consumer.New([]string{"localhost:9092"}, "my-topic", "my-group", ctx)
+	database, err := database.Conn(postgresURL)
+	if err != nil {
+		log.Fatal("Error connecting into database")
+	}
+
+	producer := producer.New([]string{kafkaURL}, kafkaTopic, ctx)
+	consumer := consumer.New([]string{kafkaURL}, kafkaTopic, kafkaGroup, ctx)
 
 	go startProducer(producer)
-	go startConsumer(consumer)
+	go startConsumer(consumer, *database)
 
 	<-sig
 	log.Println("Exiting the application...")
@@ -39,25 +54,56 @@ func startProducer(p producer.Producer) {
 			log.Fatal("Error creating UUID V7")
 		}
 
-		if err := p.WriteMessage(uuid.String(), []byte("Random Message")); err != nil {
+		msg := database.Message{ID: uuid, Value: "Random message"}
+		byteMsg, err := proto.Marshal(pb.ToProtoMessage(msg))
+		if err != nil {
+			log.Fatalf("Error in protobuf marshal")
+		}
+
+		if err := p.WriteMessage(
+			uuid.String(),
+			byteMsg); err != nil {
+			log.Print(err)
 			log.Fatalf("Message %v did not arrive", uuid.String())
 		}
-		log.Printf("Message %v arrived\n", uuid.String())
 
+		log.Printf("Message %v arrived\n", uuid.String())
 		time.Sleep(time.Second)
 	}
 }
 
-func startConsumer(c consumer.Consumer) {
+func startConsumer(c consumer.Consumer, db database.Database) {
 	for {
 		if msgs, err := c.ReadMessageBatch(5, 10); err != nil {
 			log.Fatal("Error reading batch of messages")
 		} else {
+			var batch []database.Message
+
 			log.Printf("Successful batch reading of %d messages\n", len(*msgs))
 
 			for _, msg := range *msgs {
-				log.Printf("Message %v within batch", string(msg.Key))
+				id, err := uuid.ParseBytes(msg.Key)
+				if err != nil {
+					log.Fatal("Error parsing uuid from bytes")
+				}
+
+				var protoMessage pb.Message
+				if err := proto.Unmarshal(msg.Value, &protoMessage); err != nil {
+					log.Fatalf("Error in unmarshal message %v", id)
+				}
+
+				if message, err := pb.FromProtoMessage(&protoMessage); err != nil {
+					log.Fatal("Error parsing proto message")
+				} else {
+					batch = append(batch, message)
+				}
+
+				log.Printf("Message %v within batch", id)
 			}
+
+                        log.Print(batch)
+			db.BulkMessages(batch)
+			log.Print("All messages entered into the database")
 		}
 	}
 }
