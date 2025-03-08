7fa8013679492f571facb4502c5a08dc64f679d6
rodrigocitadin <citadinrodrigo@gmail.com>
2025-03-08 15:11:36 -0300

    feat: protobuf for messages

---
+++ proto/proto.go
@@ -0,0 +1,39 @@
+package pb
+
+import (
+	"rodrigocitadin/pubsub-bulk/database"
+
+	"github.com/google/uuid"
+	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+	"gorm.io/gorm"
+)
+
+func ToProtoMessage(m database.Message) *Message {
+	return &Message{
+		Id:        m.ID.String(),
+		Value:     m.Value,
+		CreatedAt: timestamppb.New(m.CreatedAt),
+		UpdatedAt: timestamppb.New(m.UpdatedAt),
+		DeletedAt: timestamppb.New(m.DeletedAt.Time)}
+}
+
+func FromProtoMessage(pbMsg *Message) (database.Message, error) {
+	id, err := uuid.Parse(pbMsg.Id)
+	if err != nil {
+		return database.Message{}, err
+	}
+
+	msg := database.Message{
+		ID:    id,
+		Value: pbMsg.Value,
+	}
+
+	msg.CreatedAt = pbMsg.CreatedAt.AsTime()
+	msg.UpdatedAt = pbMsg.UpdatedAt.AsTime()
+
+	if pbMsg.DeletedAt != nil {
+		msg.DeletedAt = gorm.DeletedAt{Time: pbMsg.DeletedAt.AsTime(), Valid: true}
+	}
+
+	return msg, nil
+}
